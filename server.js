cc7fa8fb256acd037f05a82e9dcba03a1bc787cf
Christopher Cates <ccates1@ggc.edu>
2017-04-03 16:29:28 -0400

    Final commit

--- server.js
+++ server.js
@@ -5,25 +5,19 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://admin:starsGGCadmin@itec-gunay.duckdns.org:5984/judges');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'DELETE,GET,HEAD,POST');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+app.options(/\.*/, function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://itec-gunay.duckdns.org:5984/judges/");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+	res.sendStatus(200);
 });
-app.use(cors());
+
+app.use(cors({
+    origin: 'http://itec-gunay.duckdns.org:5984/judges/',
+    withCredentials: false,
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin' ]
+}));
+
 
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
