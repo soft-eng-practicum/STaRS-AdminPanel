71bcfce5457a01a12f6f18081b45b67439077abf
lafayette.matt <lafayette.matt@gmail.com>
2020-10-10 18:26:47 -0400

    created e2e testing framework (page objects) and mocked up 5 e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,6 +13,46 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('STaRS-Admin app is running!');
   });
 
+  it('should display important elements', () => {
+    page.navigateTo();
+
+    checkDisplayedEnabled(usernameSearch);
+    checkDisplayedEnabled(usernameInput);
+    checkDisplayedEnabled(passwordInput);
+  });
+
+  it('should block an incorrect login', () => {
+    page.navigateTo();
+
+    usernameInput.sendKeys('wronguser');
+    passwordInput.sendKeys('wrongpassword');
+
+    submitButton.click();
+    browser.sleep(200);
+    expect(errorPopup.toString().match('Invalid login credentials'));
+
+  });
+
+  it('should load and display the navigational links', () => {
+
+    elementsToTest.each(function(element) {
+      checkDisplayedEnabled(element);
+    });
+
+  });
+
+
+  it('should display data in tables', () => {
+    page.navigateTo();
+
+    posterReportLink.click();
+
+    //Make sure the tables aren't empty
+    expect(firstPosterTableRow[1].toString().not.match(''));
+    expect(firstJudgeTableRow[1].toString().not.match('')); 
+    expect (firstCombinedTableRow[1].toString().not.match(''));
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,29 @@ export class AppPage {
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
+
+  checkDisplayedEnabled(e) {
+    e.isDisplayed();
+    e.isEnabled();
+  }
+
+  usernameSearch = element(by.id('search'));
+  usernameInput = element(by.id('username'));
+  passwordInput = element(by.id('password'));
+
+  errorPopup = element(by.className('toast toast-error'));
+
+  //Need the submit button...not sure if this will work?
+  submitButton = element(by.buttonText('submit'));;
+
+  dashboardLink = element(by.css('[ui-sref="dashboard"]'));
+  posterReportLink = element(by.css('[ui-sref="posterList"]'));
+  judgeReportLink = element(by.css('[ui-sref="judgeList"]'));
+  combinedReportLink = element(by.css('[ui-sref="finalReport"]'));
+
+  elementsToTest = [this.dashboardLink, this.posterReportLink, this.judgeReportLink, this.combinedReportLink];
+
+  firstPosterTableRow = element.all(by.repeater('poster in posters')).first();
+  firstJudgeTableRow = element.all(by.repeater('judge in judges')).first();
+  firstCombinedTableRow = element.all(by.repeater('col in colContainer.renderedColumns track by col.ui')).first();
 }
